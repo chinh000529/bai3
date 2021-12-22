1db3dfc1f8dc99158cbac4352e6afe3e32061694
chinh <chinh000529@gmail.com>
2021-12-22 15:23:30 +0700

    done

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,31 +1,12 @@
-// var db = require('../db');
+var Product = require("../models/product.model");
 
-// module.exports.index = function(req, res){
-//     var page = parseInt(req.query.page) || 1;
-//     var perPage = 12;
+module.exports.index = async function (req, res) {
+  var page = parseInt(req.query.page) || 1;
+  var perPage = 8;
+  var start = (page - 1) * perPage;
 
-//     var begin = (page-1)*perPage;
-//     var end = page*perPage;
-
-//     var drop = (page-1)*perPage;
-
-//     res.render('products/index', {
-//         //products: db.get('products').value().slice(begin, end)
-//         products: db.get('products').drop(drop).take(perPage).value()
-//     });
-// };
-
-var Product = require('../models/product.model');
-
-module.exports.index = async function( req, res){
-    var page = parseInt(req.query.page) || 1;
-    var perPage = 8;
-
-    var begin = (page-1)*perPage;
-    var end = page*perPage;
-    
-    var products = await Product.find();
-    res.render('products/index', {
-        products: products.slice(begin, end)
-    });   
+  var products = await Product.find().limit(perPage).skip(start);
+  res.render("products/index", {
+    products: products,
+  });
 };
